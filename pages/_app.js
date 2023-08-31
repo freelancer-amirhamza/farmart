b474b3b0906c9e5002edc7d2bde9606e8a0b69fe
AMIR HAMZA <amir@gmail.com>
2023-08-31 21:54:41 +0600

    Project Setup

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import Layout from '@/app/layout'
+import '@/styles/globals.css'
+
+export default function App({ Component, pageProps }) {
+  return (
+    <Layout>
+       <Component {...pageProps} />
+    </Layout>
+  )
+}
